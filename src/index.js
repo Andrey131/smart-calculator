cc0f56ddbc69eba1be9f97fc6b264ca5cee0313f
Andrey131 <36641490+Andrey131@users.noreply.github.com>
2018-03-13 12:37:07 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,26 +1,35 @@
 class SmartCalculator {
   constructor(initialValue) {
-    // your implementation
+    this.term = [initialValue];
   }
 
   add(number) {
-    // your implementation
+    this.term += '+' + number;
+    return this;
   }
   
   subtract(number) {
-    // your implementation
+    this.term += '-' + number;
+    return this;
   }
 
   multiply(number) {
-    // your implementation
+    this.term += '*' + number;
+    return this;
   }
 
   devide(number) {
-    // your implementation
+    this.term += '/' + number;
+    return this;
   }
 
   pow(number) {
-    // your implementation
+    this.term += '**' + number;
+    return this;
+  }
+
+  valueOf() {
+    return eval(this.term);
   }
 }
 
